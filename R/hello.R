7ff8acc5a8115d8b2900ac786a8436d224396d01
haowangtaka <haowangtaka@gmail.com>
2023-12-01 20:14:39 -0500

    delete example

--- R/hello.R
+++ R/hello.R
@@ -3,8 +3,6 @@
 #' @description Print the hello world function to the console with
 #' parameter say who to say hello to.
 #' @param who Who to say hello to. (Default is "world")
-#' @examples
-#' hello("people in group 5 of BIS620 class")
 #' @export
 hello <- function(who = "world") {
   sprintf("Hello, %s!", who)
